cf144f6a25574057d1d9171fa6dc0835c0cf5d3d
Eric Hurst <97471253+Usmchurst@users.noreply.github.com>
2022-03-25 19:42:07 -0400

    Add files via upload
    first upload to repository

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Password-Creator
+# Password-generator-homework
+
+## Inputs for password Generation
+
+I created else if statements to ensure the generator functions correctly when the prompts ask questions.
+
+I also created variables so the data can be selected for the generator at random. 
+
+## Contributions 
+
+James Edwards
+
+Anthony Choi
+
+ Noah Brunner
+
+Seamoana Stewart
+
+Brian Samuels
---
+++ assets/script.js
@@ -0,0 +1,75 @@
+// Variables that are set
+var generateBtn = document.querySelector("#generate");
+var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+var lowercaseArr = lowercaseAlphabet.split("");
+var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var uppercaseArr = uppercaseAlphabet.split("");
+var sym = "!@#$%^&*()/?;:<>[]{}";
+var symArr = sym.split("");
+var number = "0123456789";
+var numArr = number.split("");
+
+
+
+window.onload = alert("Click to Generate password to start");
+
+ //Password Generator
+
+ function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+   passwordText.value = password;
+
+  }
+function generatePassword() {
+  var allChars = [];
+  var resultPass = "";
+
+
+// Variables set
+
+var totlength = prompt("How many characters would you like your password to be?");
+
+if(totlength <8 || totlength > 128){
+    alert("Choose a length of at least 8 characters and no more than 128 characters!/nPlease start over.");
+}
+
+else{
+  if(confirm("Would you like your password to contain lower case letters?")){
+      Array.prototype.push.apply(allChars, lowercaseArr);
+  }
+
+  if(confirm("Would you like your password to contain upper case letters?")){
+    Array.prototype.push.apply(allChars, uppercaseArr);
+}  
+
+  if(confirm("Would you like your password to contain symbols?")){
+    Array.prototype.push.apply(allChars, symArr );
+} 
+
+  if(confirm("Would you like your password to contain numbers?")){
+    Array.prototype.push.apply(allChars, numArr);
+}
+
+ if(allChars.length===0){
+  alert("You must select 1 character type to generate a password!\nPlease start over.");
+}
+ // four loop
+
+else{
+  for(var i=0; i<totlength; i++){
+      var random = Math.floor(Math.random()*allChars.length);
+      resultPass += allChars[random];
+  }
+   //Display the result
+  document.getElementById("password").innerHTML = resultPass;
+}
+}
+
+
+
+// // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
+}
+
+generatePassword();
